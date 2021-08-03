b5cc6308e5b3b3a045185d6034b10a53000f9594
fpieschaconr <fpieschaconr@unal.edu.co>
2021-08-03 10:37:24 -0500

    corrected env name convention for backend connection

--- client_frontend/src/ApolloClient.js
+++ client_frontend/src/ApolloClient.js
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: process.env.TORRETEST_URI ||"http://localhost:5000/graphql",
+  uri: process.env.REACT_APP_TORRETEST_URI ||"http://localhost:5000/graphql",
   cache: new InMemoryCache(),
 });
